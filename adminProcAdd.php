53d06d34f92cbe9f958d328fe008d335f9105748
bayupranoto139-spec <bayupranoto139@gmail.com>
2025-11-27 12:19:35 +0700

    fix show password

--- adminProcAdd.php
+++ adminProcAdd.php
@@ -2,7 +2,6 @@
 session_start();
 require "mysql.php";
 
-// proteksi admin
 if (!isset($_SESSION["role"]) || $_SESSION["role"] !== "admin") {
     die("Akses ditolak!");
 }
@@ -18,15 +17,12 @@ $foto = $_FILES["foto"]["name"];
 $tmp = $_FILES["foto"]["tmp_name"];
 $folder = "foto/" . $foto;
 
-// pindahkan file
 move_uploaded_file($tmp, $folder);
 
-// simpan ke database
-$stmt = $mysql->prepare("INSERT INTO menu (nama_menu, harga, deskripsi, kategori, foto) 
-                         VALUES (?, ?, ?, ?, ?)");
-$stmt->bind_param("sisss", $nama_menu, $harga, $deskripsi, $kategori, $foto);
+$query = "INSERT INTO menu (nama_menu, harga, deskripsi, kategori, foto)
+          VALUES ('$nama_menu', '$harga', '$deskripsi', '$kategori', '$foto')";
 
-if ($stmt->execute()) {
+if ($mysql->query($query)) {
     echo "<script>alert('Menu berhasil ditambahkan!'); window.location='adminAddMenu.php';</script>";
 } else {
     echo "<script>alert('Gagal menambahkan menu!'); history.back();</script>";
