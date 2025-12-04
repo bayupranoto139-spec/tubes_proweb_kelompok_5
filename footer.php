9d4e9f305f839ee3dbabc18418f5dad20924463d
bayupranoto139-spec <bayupranoto139@gmail.com>
2025-12-05 00:51:39 +0700

    footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <p>*Let’s stay connected:*</p>
     📍 *Location:* Jl. Pikopi No. 222
     <br>
-    📸 *IG:* @kopkop.17
+    📸 *IG:* @17coffeshop
     <br>
     ☎ *Contact:* 0865-4321-0987
     <p>© 2025 *17 COFFEE* • Crafted with elegance, enjoyed with soul🌙</p>
